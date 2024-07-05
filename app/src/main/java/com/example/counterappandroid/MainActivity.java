e970c0ac468413cf22400c61454e1331fd84eb8d
DELL <omnianabil1920@gmail.com>
2024-07-05 03:03:27 +0300

    finish simple app

--- app/src/main/java/com/example/counterappandroid/MainActivity.java
+++ app/src/main/java/com/example/counterappandroid/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.counterappandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView titleText , counterText;
+    Button addButton;
+    int counter =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        titleText = findViewById(R.id.titleText);
+        counterText = findViewById(R.id.textViewNum);
+        addButton = findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counterText.setText(""+increaseTheCounter());
+            }
+        });
+    }
+    public int increaseTheCounter(){
+       return ++counter;
     }
 }
